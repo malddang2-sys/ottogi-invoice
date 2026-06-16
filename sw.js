a4c76f06d62061703fe69d1dc5342d5e8c5534d8
malddang2-sys <malddang2@gmail.com>
2026-06-16 18:12:07 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'ottogi-v1';
+const FILES = [
+  './인수증_창대통상_1.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
